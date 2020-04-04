45f3152d4b4523ab707326f3895e9b5f0c172841
Jen Tseeng <jen@autolist.com>
2020-04-03 20:04:26 -0700

    chore: migrate to androidx

--- app/src/main/java/com/example/plainolnotes3/MainActivity.kt
+++ app/src/main/java/com/example/plainolnotes3/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.plainolnotes3
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.example.plainolnotes3.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
